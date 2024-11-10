f403921a46765f880f0fdf61e873eef80e2e0d84
sreyesg <santiagoreyes.g@gmail.com>
2024-11-09 20:02:30 -0800

    Define trackServices HTTP calls and responses

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ function App() {
   
   return (
     <>
+    <h1>This is my app</h1>
     </>
   )
 }
--- src/services/trackServices.js
+++ src/services/trackServices.js
@@ -1,7 +1,59 @@
 const BASE_URL = `${import.meta.env.VITE_BACK_END_SERVER_URL}/tracks`
 
+const index = async() => {
+    try {
+        const res = await fetch(BASE_URL) 
+        return res.json()
+        
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+// Create 
+const create = async(formData) => {
+    try {
+        res = await fetch(BASE_URL,{
+            method: "POST",
+            headers: {"content-type": "application/json"},
+            body: JSON.stringify(formData)
+        })
+        return res.json()
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+// Update
+
+const update = (formData, petId) => {
+    try {
+        res = fetch(`${BASE_URL}/${petId}`,{
+            method: "PUT",
+            headers: {"content-type":"application/json"},
+            body: JSON.stringify(formData)
+        })
+    } catch (error) {
+        
+    }
+}
+// delete
+
+const remove = async(trackId)=> {
+    try {
+        res = await fetch(`${BASE_URL}/${trackId}`, {
+            method: "DELETE"
+        })
+        return res.json()
+    } catch (error) {
+        console.log(error)
+    }
+}
 
 
 export {
-
+    index,
+    create,
+    update,
+    remove
 }
